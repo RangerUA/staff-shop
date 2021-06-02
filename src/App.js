91cea89c73f11a1fe23965b3acfde3cb574aea03
RangerUA <ramzess.zvir@gmail.com>
2021-06-02 20:20:21 +0300

    update menu-items to navigate with router HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
+import {Switch, Route, Link} from "react-router-dom";
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = (props) => {
+    console.log(props);
+  return (
+    <div>
+      <button onClick={() => props.history.push("/topics/12")}>Topic Details</button>
+      <Link to="/topics">
+        go to
+      </Link>
+      HATS PAGE
+    </div>
+  )
+
+};
+
+const HatsPageDet = (props) => {
+    console.log(props);
+    return (
+      <div>
+        <Link to="/topics">
+          go to
+        </Link>
+        HATS PAGE DET
+      </div>
+    )
+
+};
+
+const TopicsList = (props) => {
+    console.log(props);
+    return <div>TOPICS LIST PAGE</div>
+};
+
+const TopicsDetails = (props) => {
+    console.log(props);
+    return <div>TOPICS DETAILS PAGE</div>
+};
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path={"/"} component={HomePage}/>
+        <Route path={"/hats"} component={HatsPage}/>
+        <Route path={"/hats/:hatId"} component={HatsPageDet}/>
+        <Route exact path={"/topics"} component={TopicsList}/>
+        <Route path={"/topics/:topicId"} component={TopicsDetails}/>
+      </Switch>
+      {/*<HomePage/>*/}
     </div>
   );
 }
