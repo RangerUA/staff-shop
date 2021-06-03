2bc8bc25920653283ac487b43b77c8e73eaabe80
RangerUA <ramzess.zvir@gmail.com>
2021-06-03 14:07:04 +0300

    updating app with a shopPage, collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -1,55 +1,15 @@
-import {Switch, Route, Link} from "react-router-dom";
+import {Switch, Route} from "react-router-dom";
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = (props) => {
-    console.log(props);
-  return (
-    <div>
-      <button onClick={() => props.history.push("/topics/12")}>Topic Details</button>
-      <Link to="/topics">
-        go to
-      </Link>
-      HATS PAGE
-    </div>
-  )
-
-};
-
-const HatsPageDet = (props) => {
-    console.log(props);
-    return (
-      <div>
-        <Link to="/topics">
-          go to
-        </Link>
-        HATS PAGE DET
-      </div>
-    )
-
-};
-
-const TopicsList = (props) => {
-    console.log(props);
-    return <div>TOPICS LIST PAGE</div>
-};
-
-const TopicsDetails = (props) => {
-    console.log(props);
-    return <div>TOPICS DETAILS PAGE</div>
-};
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path={"/"} component={HomePage}/>
-        <Route path={"/hats"} component={HatsPage}/>
-        <Route path={"/hats/:hatId"} component={HatsPageDet}/>
-        <Route exact path={"/topics"} component={TopicsList}/>
-        <Route path={"/topics/:topicId"} component={TopicsDetails}/>
+        <Route exact path={"/shop"} component={ShopPage}/>
       </Switch>
-      {/*<HomePage/>*/}
     </div>
   );
 }
